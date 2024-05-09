4456da3761b797f111ec7c85b26e1aee07548c5d
Ethan <q40131125@gmail.com>
2024-05-09 10:18:15 +0800

    Initial Commit

---
+++ BannerWithPageControlDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BannerWithPageControlDemo
+//
+//  Created by Ethan Hung on 2024/5/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
